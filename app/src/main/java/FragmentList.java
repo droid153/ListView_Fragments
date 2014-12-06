6303bea3bedfbbbd73d7e640368c6660282a4a6c
aaomble <omble.abhijeet@gmail.com>
2014-12-05 19:23:03 -0600

    Initial Commit

---
+++ app/src/main/java/FragmentList.java
@@ -0,0 +1,21 @@
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by matrix on 10/3/2014.
+ */
+public class FragmentList extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate()
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
